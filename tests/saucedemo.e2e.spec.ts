b011b07e9fa82fb244321273a5938a039dd0bcab
kimgeonnyeong <gunnyoung22@gmail.com>
2026-03-03 16:08:24 +0900

    Update saucedemo.e2e.spec.ts

--- tests/saucedemo.e2e.spec.ts
+++ tests/saucedemo.e2e.spec.ts
@@ -13,6 +13,8 @@ test('MAIN_VERSION', async ({ page }) => {
     await page.waitForTimeout(1000);
   });
   
+  // conflict version A
+  
   // ✅ 2) 로그인 수행
   await test.step('로그인 하기', async () => {
     await page.locator('#user-name').fill('standard_user');
